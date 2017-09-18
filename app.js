acdf76caf3c431d7b9be971bc2f873afebf07962
Connor <connor.alcock.87@gmail.com>
2017-09-18 16:05:53 -0600

    Returning data in JSON format

--- app.js
+++ app.js
@@ -4,19 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/chortledb');
 
-var welcome = require('./routes/index');
+var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -31,7 +26,7 @@ app.use(function(req,res,next){
     next();
 });
 
-app.use('/', welcome);
+app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
@@ -49,7 +44,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('welcome', { title: 'Express' });
-});
 
-
-router.get('/welcome', function(req, res) {
-    res.render('welcome', { title: 'ChortlePortal!' });
-});
-
-router.get('/chortles', function(req, res) {
+router.get('/', function(req, res) {
     var db = req.db;
     var collection = db.get('chortles');
     collection.find({},{},function(e,docs){
-        res.render('chortles', {
-            "chortles" : docs
-        });
+        res.json(docs);
     });
 });
 
@@ -25,6 +15,7 @@ router.get('/newchortle', function(req, res) {
     res.render('newchortle', { title: 'Add New Chortle' });
 });
 
+/* POST to Add Chortle Service */
 router.post('/addchortle', function(req, res) {
 
     // Set our internal DB variable
@@ -34,17 +25,17 @@ router.post('/addchortle', function(req, res) {
     var userName = req.body.username;
     var userImage = req.body.userimage;
     var userMessage = req.body.usermessage;
-    var userLocation = req.body.userlocation;
+    var userLocation = req.body.userlocation
 
     // Set our collection
-    var collection = db.get('chortles');
+    var collection = db.get('chortledb');
 
     // Submit to the DB
     collection.insert({
         "username" : userName,
-        "userimage" : userImage,
-        "usermessage" : userMessage,
-        "userlocation" : userLocation
+        "image" : image,
+        "message" : message,
+        "location" : location
     }, function (err, doc) {
         if (err) {
             // If it failed, return error
